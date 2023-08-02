b1a6f6f5d07bc7d07590246b7d940f61a3387dc9
Israt-Nisha <125739039+Israt-Nisha@users.noreply.github.com>
2023-08-02 21:20:29 +0600

    Delete first_project directory

--- first_project/first_project/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse("hello, this is first django page")
-
-def about(request):
-    return HttpResponse("hello, this is about page")
